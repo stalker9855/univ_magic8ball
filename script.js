5caa01658f1a978967abdaa4b13687197aa48e63
stalker9855 <andrey.l.evseev@gmail.com>
2022-10-26 15:44:13 +0300

    lb8
    with body

---
+++ script.js
@@ -0,0 +1,40 @@
+//let body = document.querySelector('body');
+//body.innerHTML = '<div class="whiteback"><input type="text" id="input"><div id="circle"><p id="eight">8</p></div></div>';
+
+
+const ball = document.getElementById('circle');
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+const answers = ['It is certain', //1
+                'Yes — definitely', //1
+                'Most likely', //1
+                'Signs point to yes', //1 
+                'Reply hazy, try again', //1
+                'Cannot predict now', // 1
+                'My reply is no', //1
+                'Very doubtful']; //1
+
+const randomAnswer = () => answers[Math.floor((answers.length) * Math.random())];  
+
+ball.addEventListener('click', () =>{
+    console.clear();
+    console.log('I am 8-eight ball');
+    let inpt = input.value;
+    if(!inpt)
+    {
+        input.classList.add("input-shake-red");
+        input.placeholder = "Right here";
+    }
+    if(inpt)
+    {
+        input.placeholder = "Ask and push me... again";
+        input.classList.remove("input-shake-red");
+        ball.classList.add("shaker");
+        output.innerHTML = randomAnswer();
+        output.classList.add("appear");
+    }
+});
+ball.addEventListener("animationend", () => {
+    ball.classList.remove("shaker");
+    output.classList.remove("appear");
+});
